2dbdc93bb56239b2da804c1b148f13fcf3f7cf6b
nyoko708 <ckgxd325@yahoo.co.jp>
2015-11-13 23:05:09 +0900

    yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bananaRadioWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bananaRadioWebApp
+ */
+angular.module('bananaRadioWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bananaRadioWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bananaRadioWebApp
+ */
+angular.module('bananaRadioWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
