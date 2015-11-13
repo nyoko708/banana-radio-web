b0952fdd30ecd29532874d0a2fef0f47fc586be7
nyoko708 <ckgxd325@yahoo.co.jp>
2015-11-14 00:11:15 +0900

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,15 @@ var yeoman = {
 };
 
 var paths = {
-  scripts: [yeoman.app + '/scripts/**/*.js'],
+  scripts: [
+    yeoman.app + '/scripts/**/*.js',
+    yeoman.app + '/bower_components/angular/angular.js',
+    yeoman.app + '/bower_components/angular-mocks/angular-mocks.js',
+    yeoman.app + '/bower_components/angular-resource/angular-resource.js',
+    yeoman.app + '/bower_components/angular-cookies/angular-cookies.js',
+    yeoman.app + '/bower_components/angular-sanitize/angular-sanitize.js',
+    yeoman.app + '/bower_components/angular-route/angular-route.js'
+  ],
   styles: [yeoman.app + '/styles/**/*.css'],
   test: ['test/spec/**/*.js'],
   testRequire: [
